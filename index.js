12701d8d4fc0f85200e2e23ba90e8da7125257d0
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-05-14 13:04:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, cb) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(cb(theElement))
+    }
+  
+    return r;
+  }
+  function reduce(src, fnct, start){
+    let arr = (!!start) ? start : src[0];
+    let i = (!!start) ? 0 : 1 
+
+    for (i; i < src.length; i++){
+        arr = fnct(src[i], arr)
+    }
+    return arr;
+} 
