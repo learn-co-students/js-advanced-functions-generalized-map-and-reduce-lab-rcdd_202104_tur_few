e48074d2e640f2697be53382d70d8377f307ed34
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:30:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map (srcArr, func) {
+  let response = []
+  for (let i = 0; i < srcArr.length; i++) {
+      let element = srcArr[i]
+      response.push(func(srcArr[i]));
+    }
+
+    return response;
+  }
+
+  function reduce (srcArr, func, startingPoint = 0) {
+  	let resp = startingPoint;
+      for (let i = 0; i < srcArr.length; i++ ) {
+          if(srcArr[i] === false){
+              resp = false;
+              continue
+          } else if (srcArr[i] === true){
+              resp = true;
+              continue
+          }
+        resp = func(srcArr[i],resp);
+      }
+      return resp;
+  }
