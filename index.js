e72faea1e658c17a52f9b720e75021f3a40721c0
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-16 14:35:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your functions here
+
+function map(sourceArray, funct) {
+
+    let arr = [];
+
+    sourceArray.forEach(item => {
+        arr.push(funct(item));
+    });
+
+    return arr;
+}
+
+function reduce (sourceArray, funct, startingValue) {
+    let init;
+    let acc;
+
+    if (startingValue){
+        init = startingValue;
+        acc = 0;
+    } 
+    
+    else {
+        init = sourceArray[0];
+        acc = 1;
+    }
+    //could not write with .forEach since we need to skip the first item.
+    for ( ; acc < sourceArray.length; acc++) { init = funct(sourceArray[acc], init)}
+    
+    return init;
+}
