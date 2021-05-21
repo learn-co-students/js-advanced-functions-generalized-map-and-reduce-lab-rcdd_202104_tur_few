bbaff8d332072245828c80d3b839878dbff40164
DKay-code <derya.kaygisiz@outlook.com>
2021-05-21 19:54:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your functions here
+function map(src, callback) {
+    const r = [];
+
+    for (let i = 0; i < src.length; i++) {
+      const theElement = src[i];
+      r.push(callback(theElement));
+    }
+
+    return r;
+  }
+
+
+  function reduce(src, callback, startingValue) {
+    let start;
+    if (startingValue) {
+      start = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        start = callback(src[i], start);
+      }
+      return start;
+    } else {
+      start = src[0];
+      for (let i = 1; i < src.length; i++) {
+        start = callback(src[i], start);
+      }
+      return start;
+    }
+  }
+  
